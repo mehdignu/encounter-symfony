52593f10a54aec9e819e8d4e28ab9e8ea503cea2
mehdi dridi <mehdignu@users.noreply.github.com>
2019-02-06 13:34:02 +0100

    add structure of the encounter page

--- src/Controller/Welcome/HomeController.php
+++ src/Controller/Welcome/HomeController.php
@@ -6,20 +6,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
-class LoginController extends AbstractController
+
+class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="home_page")
      * @Method({"GET"})
      */
     function index(){
 
+        return $this->render('Home/index.html.twig');
 
-        return $this->render('Welcome/index.html.twig');
+    }
 
+
+    /**
+     * @Route("/encounter/{id}", name="encounter_page")
+     */
+    public function encounter($id)
+    {
+        return $this->render('Home/encounter.html.twig');
     }
 
 
